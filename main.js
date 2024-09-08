5a01c5cedfe7c5b0b78c85494d61fa99dfe6188c
cnvx94 <convexthoug94@gmail.com>
2024-09-07 23:44:16 -0600

    Audio Secret update

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-onload = () =>{
+// This function runs when the page loads
+window.onload = () => {
+        // Remove the "container" class from the body
         document.body.classList.remove("container");
-};
+    
+        // Play the audio automatically (this is optional if autoplay works in your browser)
+        var audio = document.getElementById("myAudio");
+        audio.play();
+    };
+    
